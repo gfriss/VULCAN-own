93aa025e866f16e41b45a640e9168a0161a6b059
gfriss <geri9899@gmail.com>
2024-10-10 13:54:29 +0100

    species based reduction implemented

--- reduction.py
+++ reduction.py
@@ -12,41 +12,47 @@ def calc_gj(dat, diag_sp, n_layer, spec_list):
             if sp == 'M': rate *= dat['atm']['n_0'][n_layer]
             else: rate *= dat['variable']['y'][n_layer, spec_list.index(sp)]
         if diag_sp in re_dict[re][0]:
-            loss += rate
+            loss += abs(rate)
         elif diag_sp in re_dict[re][1]:
-            gain += rate
-    if np.max([gain, loss]) == 0:
-        print('gain: ', str(gain))
-        print('loss: ', str(loss))
-        print(re_wM_dict[re])
+            gain += abs(rate)
+    #if np.max([gain, loss]) == 0:
+    #    print('gain: ', str(gain))
+    #    print('loss: ', str(loss))
+    #    print(re_wM_dict[re])
     return np.max([gain, loss])
 
-def calc_dfj(dat, diag_sp, n_layer, spec_list):
+def calc_dfj(dat, spec, diag_sp, n_layer, spec_list, dni):
     gain, loss = 0, 0
-    gain_old, loss_old = 0, 0
+    gain_delta, loss_delta = 0, 0
     for re in range(1,nr+1):
         rate = dat['variable']['k'][re][n_layer].astype(float)
-        rate_old = np.copy(rate) # no k_time sadly, so rough estimate
+        rate_delta = np.copy(rate)
         for sp in re_wM_dict[re][0]: # [0]: reactants; [1]: prodcuts
             if sp == 'M': 
                 rate *= dat['atm']['n_0'][n_layer]
-                rate_old *= dat['atm']['n_0'][n_layer]
+                rate_delta *= dat['atm']['n_0'][n_layer]
             else: 
-                rate *= dat['variable']['y_time'][-1, n_layer, spec_list.index(sp)]
-                rate_old *= dat['variable']['y_time'][-2, n_layer, spec_list.index(sp)]
+                rate *= dat['variable']['y'][n_layer, spec_list.index(sp)]
+                if sp == spec:
+                    rate_delta *= dat['variable']['y'][n_layer, spec_list.index(sp)] + dni # only veried for species i
+                else:
+                    rate_delta *= dat['variable']['y'][n_layer, spec_list.index(sp)]
         if diag_sp in re_dict[re][0]:
-            loss += rate
-            loss_old += rate_old
+            loss_delta += abs(rate_delta)
+            loss_delta += abs(rate_delta)
         elif diag_sp in re_dict[re][1]:
-            gain += rate
-            gain_old += rate_old
-    return (gain - loss) - (gain_old - loss_old)
+            gain_delta += abs(rate_delta)
+            gain_delta += abs(rate_delta)
+    return (gain_delta - loss_delta) - (gain - loss)
 
 def calc_dfj_dni(dat, spec, diag_sp, n_layer, spec_list):
-    dfj = calc_dfj(dat, diag_sp, n_layer, spec_list)
+    #dfj = calc_dfj(dat, diag_sp, n_layer, spec_list)
     dni = dat['variable']['y_time'][-1, n_layer, spec_list.index(spec)] - dat['variable']['y_time'][-2, n_layer, spec_list.index(spec)]
-    if dni == 0: dni = 1e-10
-    return dfj / dni
+    if dni == 0: # no dependency
+        return 0
+    else:
+        dfj = calc_dfj(dat, spec, diag_sp, n_layer, spec_list, dni)
+        return dfj / dni
 
 def calc_sensitivity_param(dat, spec, reduced_list, n_layer):
     B_i = 0
@@ -54,16 +60,17 @@ def calc_sensitivity_param(dat, spec, reduced_list, n_layer):
     for r_sp in reduced_list:
         ni = dat['variable']['y'][n_layer, species.index(spec)]
         gj = calc_gj(dat, r_sp, n_layer, species)
-        dfj_dni = calc_dfj_dni(dat, spec, r_sp, n_layer, species)
-        B_i += ( (ni/gj)*dfj_dni )**2
+        if gj != 0: # if this is zero it means that the species is not present at that layer
+            dfj_dni = calc_dfj_dni(dat, spec, r_sp, n_layer, species)
+            B_i += ( (ni/gj)*dfj_dni )**2
     return B_i
         
 
 vul_data = '/scratch/s2555875/output/archean.vul'
 with open(vul_data, 'rb') as handle:
     data = pickle.load(handle)
-red_spec = ['HCN']
 #%%
+red_spec = ['HCN']
 B = []
 for mol in data['variable']['species']:
     if mol not in red_spec:
@@ -77,23 +84,30 @@ for mol in data['variable']['species']:
 #plt.yscale('log')
 #%%
 again = True
-nl = 0
+nl = 100
 species_list = data['variable']['species']
 it = 0 # tracking number of iterations
+red_spec = ['HCN', 'HCN_rain', 'H2O_rain']
+B_test = []
 while again:
     it += 1
-    B, B_red = [], []
+    B, B_red, B_out = [], [], []
     for mol in species_list:
         B_new = calc_sensitivity_param(data, mol, red_spec, nl)
+        B.append(B_new)
         if mol in red_spec:
             B_red.append(B_new)
-            B.append(0) # so it will not be added again but index is kept as in species
+            B_out.append(0) # to keep its indexing and not change the max value and the total B
         else:
-            B.append(B_new)
-    if  max(B) / min(B_red) > 1e-2:
-        red_spec.append(species_list[np.argmax(B)])
+            B_out.append(B_new)
+    if  max(B_out) / min(B_red) > 1e-2:
+        red_spec.append(species_list[np.argmax(B_out)])
     else:
         again = False
         
 print('Number of iterations in layer {}: {}'.format(nl, it))
+B_sort_idx = np.argsort(B)
+B.sort(reverse = True)
+plt.plot(B, linestyle = '', marker = '.', color = 'r')
+plt.yscale('log')
 # %%
