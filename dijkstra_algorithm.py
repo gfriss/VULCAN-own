1ccc325be44b62f9261141691b083571ad6b6872
gfriss <geri9899@gmail.com>
2025-01-10 16:59:26 +0000

    avoiding loop reactions

--- dijkstra_algorithm.py
+++ dijkstra_algorithm.py
@@ -15,6 +15,7 @@ class Graph:
         # weigths of 1 and 2 through reactions b and c and total reaction rate for all reaction contributing to this edge b_t and c_t, 
         # respectively then it is 'A': {'B':[b,b_t,1], 'C':[c,c_t,2]}
         graph = {key: {} for key in self.spec_list}
+        g_reactions = {key: {} for key in self.spec_list}
         # given the  multiplicity of the network, we need to track which reactions give the weights
         # and what percantage that reaction contribute to that edge by choosing the most prominent reaction (hence the need for the total reaction rate)
         
@@ -28,21 +29,25 @@ class Graph:
             if rate == 0: # in case there is a zero value change it so no calculation errors when later taking 1/rate
                 rate = 1e-99
             # then fill up the dictionary
+            if rea[0] == rea[1]:
+                continue
             for reactant in rea[0]:
                 if reactant != 'M':
                     for product in rea[1]:
                         if product != 'M' and product in graph[reactant]: # if there is already a value for this edge, could override
-                            chosen_reaction = np.argmax([graph[reactant][product][1], rate]) # decide whether rate of new reaction is greater than previously found
-                            graph[reactant][product][0] = [graph[reactant][product][0], i][chosen_reaction] # use chosen reaction to be saved
-                            graph[reactant][product][1] += rate # adding to the total reaction rate, how to avoid duplication???
-                            graph[reactant][product][2] = [graph[reactant][product][2], 1/rate][chosen_reaction]
+                            chosen_reaction = np.argmax([g_reactions[reactant][product][1], rate]) # decide whether rate of new reaction is greater than previously found
+                            g_reactions[reactant][product][0] = [g_reactions[reactant][product][0], i][chosen_reaction] # use chosen reaction to be saved
+                            g_reactions[reactant][product][1] += rate # adding to the total reaction rate, how to avoid duplication???
+                            graph[reactant][product] = [graph[reactant][product], 1/rate][chosen_reaction]
                         elif product != 'M' and product not in graph[reactant]: # if first entry for this edge, simply assign:
-                            graph[reactant][product] = [0, 0, 0]
-                            graph[reactant][product][0] = i # reaction ID
-                            graph[reactant][product][1] = rate # total reaction rate
-                            graph[reactant][product][2] = 1/rate # weight
+                            graph[reactant][product] = 0
+                            g_reactions[reactant][product] = [0, 0]
+                            g_reactions[reactant][product][0] = i # reaction ID
+                            g_reactions[reactant][product][1] = rate # total reaction rate
+                            graph[reactant][product] = 1/rate # weight
         
         self.graph = graph
+        self.g_reactions = g_reactions
         
     def get_species(self, eq_side):
         ''' Returns the species in a given reaction in an array.'''
@@ -90,7 +95,7 @@ class Graph:
             
             for neighbour, weight in self.graph[current_node].items():
                 # Calculate the distance from current_node to the neighbor
-                tentative_distance = current_distance + weight[2]
+                tentative_distance = current_distance + weight
                 if tentative_distance < distances[neighbour]:
                     distances[neighbour] = tentative_distance
                     heappush(pq, (tentative_distance, neighbour))
@@ -99,7 +104,7 @@ class Graph:
 
         for node, distance in distances.items():
             for neighbour, weight in self.graph[node].items():
-                if distances[neighbour] == distance + weight[2]:
+                if distances[neighbour] == distance + weight:
                     predecessors[neighbour] = node
                     
         return distances, predecessors
@@ -125,16 +130,16 @@ class Graph:
             neighbour = path[i+1]
             # reaction is zeroth element in the graph, contribution is rate/total_rate but I have 1/rate (2nd item) and total rate (first item)
             # so it becomes 1/(total_rate * 1/rate)
-            reactions[self.graph[node][neighbour][0]] = [self.rea_dict[self.graph[node][neighbour][0]], 1/(self.graph[node][neighbour][1] * self.graph[node][neighbour][2]), 1/self.graph[node][neighbour][2]]
+            reactions[self.g_reactions[node][neighbour][0]] = [self.rea_dict[self.g_reactions[node][neighbour][0]], 1/(self.g_reactions[node][neighbour][1] * self.graph[node][neighbour]), 1/self.graph[node][neighbour]]
         
         return path, reactions
 # %%
 import pickle
-vul_data = 'output/a10_conv.vul'
+vul_data = '/scratch/s2555875/output/archean.vul'
 with open(vul_data, 'rb') as handle:
     data = pickle.load(handle)
 # %%
-G = Graph(data, 0)
+G = Graph(data, 16)
 #%%
 G.shortest_path('CH4', 'HCN')
 # %%
