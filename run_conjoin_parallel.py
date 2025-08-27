44db22ce091660d9aaebad05221d386be28db603
gfriss <geri9899@gmail.com>
2025-08-27 17:43:15 +0100

    upadted resource destribution

--- run_conjoin_parallel.py
+++ run_conjoin_parallel.py
@@ -8,23 +8,24 @@ import pickle
 
 rank = CW.Get_rank()
 size = CW.Get_size()
-
+# defining number of simulations per type and number of simulations per rank
 nsim = {'star': 13, 'dist': 15, 'CtoO': 15} # total number of simulations per type
 nsim_total  = 1
 for ns in nsim.values():
     nsim_total *= ns
 sim_per_rank = int(nsim_total / size) + 1 # this is needed to distribure the tasks between the CPUs, +1 makes sure there will be enough CPUs to run all sims
-
+# defining directories
 scratch = '/scratch/s2555875' # place to store outputs
 output_folder = os.path.join(scratch, 'output')
 TP_folder = os.path.join(scratch, 'TP_files/star_dist')
 conv_file = os.path.join(scratch, 'converged.txt')
 non_conv_file = os.path.join(scratch, 'non_converged.txt')
+# defining network (crahcno is defualt), only used to identify sim folders and outputs
+#network = ''
+network = '_ncho'
 # ------setting up parameterspace for all runs------
 # star type
 star_df = pf.read_stellar_data(os.path.join(scratch, 'stellar_flux/stellar_params.csv'))
-star_names = list(star_df.Name)
-T_eff = list(star_df.T_eff)
 
 C_to_O = []
 for j in range(nsim['CtoO']):
@@ -34,65 +35,66 @@ for j in range(nsim['CtoO']):
     else:
         sim_CtoO += '{}_CtoO'.format(j)
     mixing_file = os.path.join(scratch, 'mixing_files', sim_CtoO + 'mixing.txt')
-    with open(os.path.join(scratch, 'output', sim_CtoO + '.vul'), 'rb') as handle:
+    with open(os.path.join(scratch, 'output', sim_CtoO + network + '.vul'), 'rb') as handle:
         data_CtoO = pickle.load(handle)
-    C_to_O.append(pf.calc_C_to_O(data_CtoO, mixing_file))        
+    C_to_O.append(round(pf.calc_C_to_O(data_CtoO, mixing_file), 4)) # round to 4 decimal places to avoid issues with different machines
         
 # parameter dictionary of stars and distances, C/O is not inlcuded because it's same all over
 param_dict = {}
 for star,a_min,a_max in zip(star_df.Name, star_df.a_min, star_df.a_max):
     distances = np.linspace(a_min, a_max, nsim['dist'], endpoint = True)
     param_dict[star] = list(distances)
-# defining network (crahcno is defualt), only used to identify sim folders and outputs
-#network = ''
-network = '_ncho'
 # Boolian to check convergence and rerun if needed
 check_conv = True
+# destributing resources, so finding sims that have not been run yet (needed if rerunning after crash or wanting to redistribute)
+sim_done = []
+if os.isfile(os.path.join(scratch, 'star_dist_CtoO_rain.txt')):
+    with open(os.path.join(scratch, 'star_dist_CtoO_rain.txt'), 'r') as f:
+        for line in f:
+            if line[0].isnumeric():
+                bits = line.split()[:3] # keeping Teff, dist and CtoO
+                star_name = star_df.loc[star_df.T_eff == float(bits[0]), 'Name'].iloc[0]
+                i_dist = param_dict[star_name].index(float(bits[1]))
+                i_CtoO = C_to_O.index(float(bits[2]))
+                sim_done.append((star_name, i_dist, i_CtoO))
+sim_not_done = []
+for i in range(nsim_total):
+    i_star = i//(nsim['dist']*nsim['CtoO']) # which star (changes after looped through all distance and C/O possibilities)
+    star_name = star_df.Name.loc[i_star]
+    i_dist = (i//nsim['CtoO'])%nsim['dist'] # which distance (changes after looped through all C/O possibilities and then restarts when all distances are done)
+    i_CtoO = i%nsim['CtoO'] # which C/O (simply loops through the C/O possibilities)
+    if (star_name, i_dist, i_CtoO) not in sim_done:
+        sim_not_done.append((star_name, i_dist, i_CtoO))
+#sim_not_done = sim_not_done[::-1]
+sim_per_rank = int(len(sim_not_done) / size) + 1 # this is needed to distribure the tasks between the CPUs, +1 makes sure there will be enough CPUs to run all sims
 # ------end of parameter set up-----
 # ------start of simulation loop------
 for i in range(rank*sim_per_rank, (rank+1)*sim_per_rank):   # paralellisation itself, it spreads the task between the CPUs
                                                             # this is the magic, after this just think of it as a normal, sequential loop
-    if i > nsim_total - 1: # in case using total sim number is not divisible by number of CPUs
+    if i > len(sim_not_done) - 1: # in case using total sim number is not divisible by number of CPUs
         continue
-    i_star = i//(nsim['dist']*nsim['CtoO']) # which star (changes after looped through all distance and C/O possibilities)
-    i_dist = (i//nsim['CtoO'])%nsim['dist'] # which distance (changes after looped through all C/O possibilities and then restarts when all distances are done)
-    i_CtoO = i%nsim['CtoO'] # which C/O (simply loops through the C/O possibilities)
+    s_name = sim_not_done[i][0] # which star (changes after looped through all distance and C/O possibilities)
+    i_dist = sim_not_done[i][1] # which distance (changes after looped through all C/O possibilities and then restarts when all distances are done)
+    i_CtoO = sim_not_done[i][2] # which C/O (simply loops through the C/O possibilities)
     # build simulation and folder names
-    sim = 'star_{}'.format(star_names[i_star]) # param matrix first goes through the stars
+    sim = 'star_{}'.format(s_name) # param matrix first goes through the stars
     dist_sim = 'dist_' + pf.get_str_number(i_dist) # then the distance
     CtoO_sim = 'CtoO_' + pf.get_str_number(i_CtoO) # then the C/O ratio
     TP_sim = '_'.join([sim, dist_sim]) # TP profile is the same for all C/O ratios
     sim = '_'.join([sim, dist_sim, CtoO_sim]) # this is the final simulation name
     sim_folder = os.path.join(scratch, sim + network)
-    # skip parts that are already done, i.e. all distance with base C/O (0th) (same name as used for TP profile)
-    # but add their output to the file
-    # to be deleted later
-    #with open(conv_file, 'r') as f:
-    #    conv_text = f.read()
-    #if sim + network + '.vul' in conv_text:
-    #    continue
-    #with open(non_conv_file, 'r') as f:
-    #    non_conv_text = f.read()
-    #if sim+network+'_rerun.vul' in non_conv_text:
-    #    continue # don't do it again if it didn't converge the first time
-    #if i_CtoO == 0:
-    #    with open(os.path.join(output_folder, TP_sim+network+'.vul'), 'rb') as handle:
-    #        data = pickle.load(handle)
-    #    hcn_rain = pf.rainout(data, rain_spec = 'HCN_rain', g_per_mol = 27)
-    #    h20_rain = pf.rainout(data, rain_spec = 'H2O_rain', g_per_mol = 18)
-    #    with open(os.path.join(scratch, 'star_dist_CtoO_rain.txt'), 'a') as f:
-    #        f.write('{}\t{}\t{}\t{}\t{}\n'.format(T_eff[i_star], param_dict[star_names[i_star]][i_dist], C_to_O[i_CtoO], hcn_rain, h20_rain))
-    #    continue
+    if os.path.isdir(sim_folder):
+        continue
     # build files for simulation
     out_file = sim + network + '_nowash.vul'
     out_change = ','.join(['out_name', out_file, 'str'])
     output_dir_change = ','.join(['output_dir', output_folder, 'str'])
     new_cfg = os.path.join(sim_folder, 'vulcan_cfg.py')
-    new_rad_file = pf.get_rad_prof(star_names[i_star])
+    new_rad_file = pf.get_rad_prof(s_name)
     rad_file_change = ','.join(['sflux_file', new_rad_file, 'str'])
-    new_r_star = str(star_df.loc[star_df.Name == star_names[i_star]].R.iloc[0])
+    new_r_star = str(star_df.loc[star_df.Name == s_name].R.iloc[0])
     r_star_change = ','.join(['r_star', new_r_star, 'val'])
-    new_orbit_radius = str(param_dict[star_names[i_star]][i_dist])
+    new_orbit_radius = str(param_dict[s_name][i_dist])
     orbit_radius_change = ','.join(['orbit_radius', str(new_orbit_radius), 'val'])
     new_tp_file = os.path.join(TP_folder, TP_sim+'.txt')
     tp_file_change = ','.join(['atm_file', new_tp_file, 'str'])
@@ -138,7 +140,7 @@ for i in range(rank*sim_per_rank, (rank+1)*sim_per_rank):   # paralellisation it
         hcn_rain = pf.rainout(data, rain_spec = 'HCN_rain', g_per_mol = 27)
         h20_rain = pf.rainout(data, rain_spec = 'H2O_rain', g_per_mol = 18)
     with open(os.path.join(scratch, 'star_dist_CtoO_rain.txt'), 'a') as f:
-        f.write('{}\t{}\t{}\t{}\t{}\n'.format(T_eff[i_star], param_dict[star_names[i_star]][i_dist], C_to_O[i_CtoO], hcn_rain, h20_rain))
+        f.write('{}\t{}\t{}\t{}\t{}\n'.format(star_df.loc[star_df.Name == s_name, 'T_eff'].iloc[0], param_dict[s_name][i_dist], C_to_O[i_CtoO], hcn_rain, h20_rain))
     # then exit simulation folder and delete it along with output and cfg file
     os.chdir(wd)
     subprocess.check_call(['rm', '-rf', sim_folder])
